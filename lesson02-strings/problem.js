b6a9c99a1975a91437228f70fd9f002e6af92434
Tafari Excell <excell.tafari@gmail.com>
2025-01-25 20:25:43 -0500

    Lesson02 completed

--- lesson02-strings/problem.js
+++ lesson02-strings/problem.js
@@ -4,8 +4,31 @@
  * @param {string} s
  * @return {number}
  */
-function lengthOfLongestSubstring(s) {
-  // Your code here
+
+//input = string or empty string
+//output = integer representing the length of the longest substring without repeating characters
+//constraints = empty string should return 0, no repeating characters should return the length of the string
+//edge cases = empty string, string with no repeating characters, string with repeating characters
+//approach = use a set to keep track of unique characters in the current substring
+//time complexity = O(n) where n is the length of the string
+//space complexity = O(n) where n is the length of the string
+function lengthOfLongestSubstring(s){
+  let sub = new Set();
+  let left = 0;
+  let longest = 0;
+
+  for (let right = 0; right < s.length; right++){
+    while(sub.has(s[right])) {
+      sub.delete(s[left])
+      left++
+    }
+  
+    sub.add(s[right])
+    longest = Math.max(longest, right - left + 1)
+  }
+  return longest
 }
+
+console.log(lengthOfLongestSubstring('school'))
 
 module.exports = lengthOfLongestSubstring;
--- lesson02-strings/warmup.js
+++ lesson02-strings/warmup.js
@@ -1,17 +1,17 @@
-// 1. given a string with three characters, return the second character
+// 1. given a string with three characters, return the second character check
 
-// 2. get the second and third char from a string. the string will always have atleast three characters
+// 2. get the second and third char from a string. the string will always have atleast three characters check
 
 // 'cat' => 'at'
 // 'robbed' => 'ob'
 
-// 3. Given a word with 5 letters, return the second through 5th letter
+// 3. Given a word with 5 letters, return the second through 5th letter check 
 
 // function getSecondThroughFifthLetter (word){
 //   return word.slice(1);
 // }
 
-// 4. given a string and a number called N, return the second through the Nth character
+// 4. given a string and a number called N, return the second through the Nth character check
 
 // 'schoolbus', 5 => 'choo'
 // 'telephone', 6 => 'eleph
@@ -19,18 +19,18 @@
 // slice is a ?? method: array and also a string method
 // splice is a array method
 
-// 5. Given an array of booleans, return an arrray of only false elements
+// 5. Given an array of booleans, return an arrray of only false elements check
 
 // [true, true, false, true , false] => [false, false]
 
-// 6. given a string with two words, put ' the ' between the words and return the string
+// 6. given a string with two words, put ' the ' between the words and return the string check
 
 // 'to beach' => 'to the beach'
 // 'for real' => 'for the real'
 // 'jim john' => 'jim the john'
 // 'to house' => 'to the house'
 
-// 7.  given two numbers, price and discount, return the price after the discount is applied
+// 7.  given two numbers, price and discount, return the price after the discount is applied check
 
 // price is 100 and discount is 15
 
@@ -39,3 +39,48 @@
 // 70, 3 => 67.9
 
 // Console.log each of your answers with the test cases provided. Attempt to write each one with TDD.
+
+// function secondChar(str) {
+//     return str.slice(1,2)
+// }
+// console.log(secondChar("robbed"))
+
+// function secondAndThirdChar(str) {
+//     if (str.length >= 3){
+//         return str.slice(1,3)
+//     }
+// }
+// console.log(secondAndThirdChar("cat"))
+
+// function getSecondThroughFifthLetter(word){
+//     return word.slice(1,5)
+// }
+// console.log(getSecondThroughFifthLetter("robbed"))
+
+// function secondToNthChar(str, n) {
+// return str.slice(1,n)
+// }
+// console.log(secondToNthChar("telephone", 6))
+
+// function onlyFalse(arr) {
+// let res = []
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] === false) {
+//         res.push(arr[i])
+//     }
+//     }
+//     return res
+// }
+// console.log(onlyFalse([true, true, false, true, false]))
+
+// function addThe(str) {
+// return str.replace(/\s/g, " the ")
+
+// }
+
+// console.log(addThe('to house'))
+
+// function discountPrice(price , discount){
+// return price - (discount / 100 * price) 
+// }
+// console.log(discountPrice(50, 30))
